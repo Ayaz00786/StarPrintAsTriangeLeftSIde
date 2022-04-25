d27bcf2367ca700e4348b8d98bc700791f0e32f0
Ayaz <Ayaz@DESKTOP-5QKM9AK>
2022-04-25 13:55:24 +0530

    Add project files.

---
+++ StarPrintAsTriangeLeftSIde/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StarPrintAsTriangeLeftSIde
+{
+    internal class Class1
+    {
+        public static void starprint()
+        {
+            for (int i = 6; i >= 1; i--)
+            {
+                for(int j = 1; j <= i; j++)
+                {
+                    Console.Write(" * ");
+                }
+               Console.WriteLine();
+            }
+            //Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StarPrintAsTriangeLeftSIde/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StarPrintAsTriangeLeftSIde
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1.starprint();
+        }
+    }
+}
